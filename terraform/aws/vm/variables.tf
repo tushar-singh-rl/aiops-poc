a37a612facb9dc6330864b35aeb0d2619e19989a
tushar-singh-rl <tushar.singh@relevancelab.com>
2025-06-16 23:31:50 +0530

    Added aws setup

---
+++ terraform/aws/vm/variables.tf
@@ -0,0 +1,23 @@
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+  default = "c666cf43-3fb9-4c12-aa1c-b17a4275a58e"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure client ID"
+  default = "0d55f151-d9f5-4c4f-82af-544e0265595c"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure client secret"
+  default = "i_~8Q~LePJbVfbSphTpaSv3DPn.lwHZIcq7skc2Q"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure tenant ID"
+  default = "398de9f4-7b9b-4bec-b0ed-69737fa45cf4"
+}
