dd68261a7522d3afb2c2d301498dc583f979d6e2
tushar-singh-rl <tushar.singh@relevancelab.com>
2025-06-16 13:34:21 +0530

    Updated variable

---
+++ terraform/azure/vm/variables.tf
@@ -0,0 +1,19 @@
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "client_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "client_secret" {
+  type        = string
+  description = "Azure subscription ID"
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure subscription ID"
+}
